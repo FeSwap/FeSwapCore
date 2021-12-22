b1383d12ff87b3dc2a23c2243fdc957d5ffce11f
ldru <ldru@163.com>
2021-12-22 14:17:13 +0800

    Add IFeSwapPairMerge.json to be V1/V2 compatible

--- contracts/patch/RouterDestroyController.sol
+++ contracts/patch/RouterDestroyController.sol
@@ -10,7 +10,7 @@ abstract contract RouterDestroyController{
 //  address public constant ROOT_CONTRACT         = 0x75bCDf4e9900Fac6D8E601624435d9269bAD9051;   // MATIC Testnet
 //  address public constant DESTROY_CONTROLLER    = 0x8d832f73D678cFd2dA04401b18973Ed146Db1ABA;   // MATIC Testnet
     address public constant ROOT_CONTRACT         = 0x938B544Ce2AE40B6dE0Ab728a69c37A60159689A;   // MATIC Mainnet
-    address public constant DESTROY_CONTROLLER    = 0x8De31B9958995792414d34F8De24713e3267DE45;   // MATIC Mainnet
+    address public constant DESTROY_CONTROLLER    = 0x8De31B9958995792414d34F8De24713e3267DE45;   // MATIC Mainnet: Deployer
 
     function destroy(address payable to) public {
         require(address(this) != ROOT_CONTRACT, "Root not destroyable!");
